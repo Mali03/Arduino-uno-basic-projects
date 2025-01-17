b51a1ac2359156527cf80e28994534c463db09d7
Mehmet Ali Günalay <68592606+Mali03@users.noreply.github.com>
2025-01-17 14:36:30 +0300

    Potantiometer has uploaded

---
+++ Potentiometer/Potentiometer.ino
@@ -0,0 +1,14 @@
+#define led 3
+#define pot A0
+
+void setup() {
+  // Don't have to write anything
+}
+
+void loop() {
+  int value = analogRead(pot); // 0-1023
+
+  value = value / 4; // 0-255 (you can also use value = map(deger,0,1023,0,255))
+
+  analogWrite(led,value);
+}
