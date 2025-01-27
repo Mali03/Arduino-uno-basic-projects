b373867e4ec9034dce941654a1b4eb9a6435d919
Mehmet Ali Günalay <68592606+Mali03@users.noreply.github.com>
2025-01-27 08:55:07 +0300

    activeAndPassiveBuzzer has been added

---
+++ activeAndPassiveBuzzer/activeAndPassiveBuzzer.ino
@@ -0,0 +1,23 @@
+#define buzzer 2
+
+void setup() {
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  // for active buzzer
+  /*
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  delay(100);
+  */
+
+  // for passive buzzer
+  /*
+  tone(buzzer,1000);
+  delay(100);
+  noTone(buzzer);
+  delay(100);
+  */
+}
